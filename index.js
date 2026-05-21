c750d95ad22892429ba1742b29551e459ba755a6
007shishir <islam7.saiful@gmail.com>
2026-05-21 18:42:53 +0600

    make vercel ready

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Select the studyNook database and target collection
     const db = client.db("study-nook");
@@ -582,10 +582,10 @@ app.patch('/api/bookings/:id/cancel', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!",
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
